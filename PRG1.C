448e2cfe8be5bfa4b69be656e5d8b45e715ffbab
moruumavenkatesh-cmyk <moruumavenkatesh@gmail.com>
2025-09-15 20:54:55 +0530

    Add files via upload
    check out for other programes

---
+++ PRG1.C
@@ -0,0 +1,9 @@
+//program to show the usage of comment lines
+#include<stdio.h>
+void main()
+{
+printf("\n this is the first line in my c programe");
+//printf("\n this is second line in my c programe");
+//printf("\n this is third line in my c programe");
+printf("\n this is the fourth line in my c programe");
+}
