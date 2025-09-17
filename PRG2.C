5f4b82f03cb5d7a5ffbcf8f9cd82575891e67360
raja balichakravarthi <moruumavenkatesh@gmail.com>
2025-09-17 23:11:34 +0545

    Add files via upload

---
+++ PRG2.C
@@ -0,0 +1,10 @@
+//this program to show how to clear screen in turboc using clrscr()
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+printf("\n this line print before the screen clear");
+clrscr();
+printf("\n this line print after the screen clear");
+getch();
+}
