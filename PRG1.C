76623f9aa466eb6715a3c8bd35dfa446df06befb
raja balichakravarthi <moruumavenkatesh@gmail.com>
2025-09-20 20:57:22 +0530

    Update PRG1.C

--- PRG1.C
+++ PRG1.C
@@ -6,4 +6,7 @@ printf("\n this is the first line in my c programe");
 //printf("\n this is second line in my c programe");
 //printf("\n this is third line in my c programe");
 printf("\n this is the fourth line in my c programe");
+
 }
+
+//DEVIL'S CODE
