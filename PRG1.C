0955ef8bdea66caea61a6c166b0f65bf3b3e4920
raja balichakravarthi <moruumavenkatesh@gmail.com>
2025-09-20 20:51:26 +0530

    Delete PRG1.C

--- PRG1.C
+++
@@ -1,9 +0,0 @@
-//program to show the usage of comment lines
-#include<stdio.h>
-void main()
-{
-printf("\n this is the first line in my c programe");
-//printf("\n this is second line in my c programe");
-//printf("\n this is third line in my c programe");
-printf("\n this is the fourth line in my c programe");
-}
