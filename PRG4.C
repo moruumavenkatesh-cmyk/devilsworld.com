99e0b87cf7f0d710264a97245e9eacbf898d180d
raja balichakravarthi <moruumavenkatesh@gmail.com>
2025-09-20 21:17:57 +0545

    Add files via upload

---
+++ PRG4.C
@@ -0,0 +1,13 @@
+//program example for declaring and initializing int data type variables
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int no1=10;
+int no2;
+clrscr();
+printf("\n enter any value for no2:");
+scanf("%d",&no2);
+printf("\n the value in no1 is :%d\n the value in no2 is :%d",no1,no2);
+getch();
+}
