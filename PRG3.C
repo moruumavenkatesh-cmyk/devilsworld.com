23d346712cc0efe2cc83f509e6ef2e1d63a95db3
raja balichakravarthi <moruumavenkatesh@gmail.com>
2025-09-20 21:16:20 +0545

    Add files via upload

---
+++ PRG3.C
@@ -0,0 +1,12 @@
+//this program to show the usage of escape sequence characters
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("\n this is first line\t in my c programe\b\a");
+printf("\n this is second line in my c programe");
+printf("\n this i third line in my c programe");
+printf("\n this is fourth line in my c programe");
+getch();
+}
